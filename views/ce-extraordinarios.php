051661c96f1f8ee27bd19b0bb742d4f81d0b255a
Joel <elsuperjoy@gmail.com>
2023-09-22 12:30:32 -0600

    agrege calendario

--- views/ce-extraordinarios.php
+++ views/ce-extraordinarios.php
@@ -258,7 +258,7 @@ $resultadoAsignatura = $asignatura->consultarId();
                           <?php
                             if (($resultadoCalificacion["data"][0]["calificacion"] >= $resultadoPrograma["data"]["calificacion_aprobatoria"]) 
                             || (!isset($resultadoCalificacion["data"][0]["calificacion"]))
-                            || ($resultadoAlumno["data"]["situacion_id"] == 3)
+                            || ($resultadoAlumno["data"]["situacion_id"] == 3) || ($resultadoAlumno["data"]["situacion_id"] == 4)
                             ) { echo "readonly"; }
                           ?> />
                         </td>
